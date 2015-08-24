d5ecf422289eb28d65705824e273dfa8d168fd60
wangcj <wangcj@mail.jj.cn>
2015-08-24 18:37:06 +0800

    slice 练习

--- slice.go
+++ slice.go
@@ -7,7 +7,25 @@ import (
 
 func main() {
 	sl_0 := make([]int,10)
-	sl_0[9]=12
+	for a:=1;a<10;a++{
+		sl_0[a]=a;
+	}
+	sl_1 := sl_0[1:5]
 	fmt.Println(sl_0)
+	fmt.Println(sl_1)
+	fmt.Println(sl_0[:5])
+	fmt.Println(sl_0[1:])
+	fmt.Println(sl_0[:])
+
+fmt.Println()
+	fmt.Println(sl_0)
+	fmt.Println(sl_1)
+	fmt.Println()	
+
+	s0 :=[]int{1,2}
+//	sl_1 = append()
+	fmt.Println(s0)
+
+
 	fmt.Println("Hello World!")
 }
