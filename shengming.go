e0428732b76390813de7373b1ae96a71bc0b253e
wangcj <wangcj@mail.jj.cn>
2015-08-25 17:41:48 +0800

    批量声明

---
+++ shengming.go
@@ -0,0 +1,30 @@
+// 声明
+package main
+
+import (
+	"fmt"
+)
+
+const(
+	pi =3.14
+	c =100
+	x ="apple"
+)
+
+var(
+	a = 100
+	b = "mysql"
+	f = "dell"
+)
+
+type(
+	newType int
+	type1 float64
+	type2 bool
+)
+
+func main() {
+	fmt.Println("Hello World!")
+	fmt.Println(x)
+	fmt.Println(f)
+}
