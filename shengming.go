95a0271b1ef19b35fc14d1ca2f55786bee2214d6
wangcj <wangcj@mail.jj.cn>
2015-08-26 13:58:33 +0800

    类型声明

--- shengming.go
+++ shengming.go
@@ -26,10 +26,16 @@ type(
 )
 
 func main() {
+	var (
+		a6 = 1
+	)
+	var a5 int
 	a3,a4 := "3","*"
 	fmt.Println("Hello World!")
 	fmt.Println(x)
 	fmt.Println(f)
 	fmt.Println(a3)
 	fmt.Println(a4)
+	fmt.Println(a5)
+	fmt.Println(a6)
 }
