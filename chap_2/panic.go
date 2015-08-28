397460844d1f31a368abfccbb5595ccb6d060d44
qqzj <popxmz@Gmail.com>
2015-08-28 15:51:28 +0800

    整理目录结构

--- chap_2/panic.go
+++ chap_2/panic.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 	fmt.Println("你猜会不会出错?")
-	defer func(){
-		if err:=recover();err!=nil{
+	defer func() {
+		if err := recover(); err != nil {
 			fmt.Println("真的出错了")
 			fmt.Println(err)
 			fmt.Println("然后,就没有然后了...")
@@ -21,10 +21,12 @@ func main() {
 	runSome(true)
 	fmt.Println("幸福快乐的生活在一起...")
 }
-func runSome(rmm bool){
+func runSome(rmm bool) {
 	fmt.Println("今天天气好晴朗")
 	fmt.Println("处处好风光")
-	if rmm {panic("卧槽,容嬷嬷来了")}
+	if rmm {
+		panic("卧槽,容嬷嬷来了")
+	}
 	fmt.Println("好风光")
 	fmt.Println("马蹄忙,蜜蜂也忙")
 }
