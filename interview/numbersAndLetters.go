a5549a388c76fd17cfd6db347b03b86537024147
wangchaojie <popxmz@Gmail.com>
2020-03-18 23:54:48 +0800

    面试题: 交叉打印数字与字母

---
+++ interview/numbersAndLetters.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg *sync.WaitGroup
+	var letter chan bool
+	var number chan bool
+	wg = &sync.WaitGroup{}
+	wg.Add(1)
+	letter, number = make(chan bool), make(chan bool)
+
+	go func() {
+		var i int
+		i = 1
+		for {
+			select {
+			case <-number:
+				fmt.Printf("[%d,%d] ", i, i+1)
+				i = i + 2
+				letter <- true
+				if i > 24 {
+					goto endNumber
+				}
+				break
+			default:
+				break
+			}
+		}
+	endNumber:
+		fmt.Printf("%s", "{数字输出完毕, goroutine退出} ")
+		return
+	}()
+
+	go func(wg *sync.WaitGroup) {
+		var str string
+		var i int
+		var count int
+		str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		i = 0
+		count = len(str)
+		for {
+			select {
+			case <-letter:
+				if i < count-2 {
+					fmt.Printf("[%s,%s] ", str[i:i+1], str[i+1:i+2])
+					i = i + 2
+					if i > 23 {
+						goto endLetter
+					}
+					number <- true
+				}
+				break
+
+			default:
+				break
+			}
+		}
+	endLetter:
+		fmt.Printf("%s", "{字母输出完毕, goroutine退出} ")
+		wg.Done()
+		return
+	}(wg)
+	number <- true
+	wg.Wait()
+}
